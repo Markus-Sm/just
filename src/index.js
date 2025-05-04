cf8772062c42932246190dac3d94e9dc30ee88a9
Marek Samofał <96687413+Markus-Sm@users.noreply.github.com>
2025-05-04 17:37:57 +0200

    nest component

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ function Greeting(){
     return <h3>Hello There</h3>
 }
 
+const Person = () => <h2>Mark Samofał</h2>
+
+const Message = () => <p>Thank you for visiting my website</p>
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(<Greeting />);
