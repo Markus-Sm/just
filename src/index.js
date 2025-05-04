ccf9098d6c308fff86004a883473ca192f848169
Marek Samofał <96687413+Markus-Sm@users.noreply.github.com>
2025-05-04 17:49:49 +0200

    book list 01

--- src/index.js
+++ src/index.js
@@ -15,6 +15,20 @@ const Person = () => <h2>Mark Samofał</h2>
 
 const Message = () => <p>Thank you for visiting my website</p>
 
+const Book = () => {
+    return (
+        <article>
+            hello
+        </article>
+    )
+}
+
+const Image = () => <h2>Image placeholder</h2>;
+
+const Title = () => <h2>Book Title</h2>;
+
+const Author = () => <h2>Book Author</h2>;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(<Greeting />);
