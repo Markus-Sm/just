d65b08a0b1da98c8b044b9d01828e6525a6877c8
Marek Samofał <96687413+Markus-Sm@users.noreply.github.com>
2025-05-04 15:48:31 +0200

    update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function Greeting(){
-    return <h3>Hello§§</h3>
+    return <h3>Hello There</h3>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
