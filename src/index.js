87b0f64727e3dfdcc63ef1b54c36a16221aeac4e
Marek Samofał <96687413+Markus-Sm@users.noreply.github.com>
2025-05-04 21:05:35 +0200

    props 02

--- src/index.js
+++ src/index.js
@@ -3,31 +3,38 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-const title = "The Let Them Theory";
-const author = "Mel Robbins";
-const img = "./images/book-1.jpg";
+const firstBook = {
+    title: "The Let Them Theory",
+    author: "Mel Robbins",
+    img: "./images/book-1.jpg",
+}
+
+const secondBook = {
+    title: "Sunrise on the Reaping",
+    author: "Suzanne Collins",
+    img: "./images/book-2.jpg",
+}
+
+const thirdBook = {
+    title: "Mom, I Want to Hear Your Story",
+    author: "Jeffrey Mason",
+    img: "./images/book-3.jpg",
+}
 
 const BookList = () => {
     return <section className="booklist">
-        <Book job="developer" />
-        <Book title="random" number={22} />
-        <Book />
-        <Book />
-        <Book />
-        <Book />
+        <Book {...firstBook} />
+        <Book {...secondBook} />
+        <Book {...thirdBook} />
     </section>
 }
 
 const Book = (props) => {
-    console.log(props)
     return (
         <article className="book">
-            <img src={img} alt={title} />
-            <h2>{title}</h2>
-            <h4>{author}</h4>
-            <p>{props.job}</p>
-            <p>{props.title}</p>
-            <p>{props.number}</p>
+            <img src={props.img} alt={props.title} />
+            <h2>{props.title}</h2>
+            <h4>{props.author}</h4>
         </article>
     )
 }
