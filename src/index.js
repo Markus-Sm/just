6eb7f1e1667b61bc9332201eb90c2795eed3f6ab
Marek Samofał <96687413+Markus-Sm@users.noreply.github.com>
2025-05-05 09:51:05 +0200

    update

--- src/index.js
+++ src/index.js
@@ -3,39 +3,46 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-const firstBook = {
+const books = [
+  { 
     title: "The Let Them Theory",
     author: "Mel Robbins",
     img: "./images/book-1.jpg",
-}
-
-const secondBook = {
+    children: "lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+  },
+  {
     title: "Sunrise on the Reaping",
     author: "Suzanne Collins",
     img: "./images/book-2.jpg",
-}
-
-const thirdBook = {
+  },
+  {
     title: "Mom, I Want to Hear Your Story",
     author: "Jeffrey Mason",
     img: "./images/book-3.jpg",
-}
+  },
+]
+
+const names = ["John", "Peter", "Jacob"];
+const newNames = names.map((name) => {
+    console.log(name)
+})
+
 
 const BookList = () => {
     return <section className="booklist">
-        <Book {...firstBook} />
-        <Book {...secondBook} />
-        <Book {...thirdBook} />
+        {names}
     </section>
 }
 
 const Book = (props) => {
-    const {img, title, author} = props;
+    const {img, title, author, children} = props;
+    console.log(props);
     return (
         <article className="book">
             <img src={img} alt={title} />
             <h2>{title}</h2>
             <h4>{author}</h4>
+            <p>{children}</p>
         </article>
     )
 }
